64ecb5f1a999f3c73f988f2520eb8ce27d97797e
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2025-01-07 18:52:00 -0500

    always force audio sampler to 48kHz

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -7,12 +7,8 @@
 #include <signal.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#ifndef WIIU
-#define AUDIO_FREQUENCY 384000
-#else
-/* Use the internal sample rate for the Wii U */
+
 #define AUDIO_FREQUENCY 48000
-#endif
 
 #ifdef _WIN32
 #include <direct.h>
